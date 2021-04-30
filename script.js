c8eb05684c6d741d0c194ee7a02944b08c881eb6
Artem Grinko <asgrinko@alfabank.ru>
2021-04-30 16:56:51 +0300

    спрайт бомбермена

--- script.js
+++ script.js
@@ -2,6 +2,7 @@ const width = 900;
 const height = 600;
 const rows = 10; // рядов
 const columns = 15; // столбцов
+let player;
 let game;
 
 const GRASS = new Image();
@@ -16,7 +17,32 @@ BRICKS.src = "images/bricks.png";
 window.onload = function() {
     console.log("Скрипты подключены");
     createCanvas();
+    loadPlayer();
     requestAnimationFrame(render);
+}
+
+function loadPlayer() {
+    const playerSprite = new Sprite({
+        frameHeight: 80,
+        frameWidth: 62,
+        animations: {
+            DOWN: { x: 1, y: 0, frameCount: 3 },
+            RIGHT: { x: 188, y: 0, frameCount: 3 },
+            UP: { x: 2, y: 84, frameCount: 3 },
+            EXPLOSION: { x: 0, y: 380, frameCount: 6 },
+            // TODO: LEFT
+        },
+        timeout: 100,
+        image: "images/bomberman.png"
+    });
+    player = {
+        sprite: playerSprite,
+        x: 66,
+        y: 66,
+        width: 48,
+        height: 60,
+        state: 'EXPLOSION'
+    }
 }
 
 function createCanvas() {
@@ -31,6 +57,14 @@ function createCanvas() {
 function render() {
     requestAnimationFrame(render);
     renderWorld();
+    renderPlayer();
+}
+
+function renderPlayer() {
+    const { sprite, state } = player;
+    const frame = sprite.getFrame(state);
+    game.drawImage(frame.image, frame.x, frame.y, frame.w, 
+            frame.h, player.x, player.y, player.width, player.height);
 }
 
 function renderWorld() {
